4938293c92f96b5775361dd6d6edbb6fc97aef30
EdwardS02 <edwardsantos405.gx@gmail.com>
2025-05-10 00:02:02 -0400

    Slice practice

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var yourName = prompt("What is your name?");
-var message = prompt("Hey " + yourName + " tell me a little about yourself: ");
-alert("You have written " + message.length + " characters, you have " + (140 - message.length) + " characters left.");
+var user = prompt("Username: ");
+var tweet = prompt("What's on your mind? (max 280 characters)");
+alert(user + " wrote: " + tweet.slice(0,280) + " (You typed " + tweet.length + " out of 280 characters)");
