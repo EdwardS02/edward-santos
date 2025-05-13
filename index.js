25f37a5d4722a09e44e88b0dfef8533d17a793cd
EdwardS02 <edwardsantos405.gx@gmail.com>
2025-05-13 06:10:21 -0400

    basic math practice

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-var user = prompt("Username: ");
-var tweet = prompt("What's on your mind? (max 280 characters)");
-alert(user + " wrote: " + tweet.slice(0,280) + " (You typed " + tweet.length + " out of 280 characters)");
+// var user = prompt("Username: ");
+// var tweet = prompt("What's on your mind? (max 280 characters)");
+// alert(user + " wrote: " + tweet.slice(0,280) + " (You typed " + tweet.length + " out of 280 characters)");
+
+// var name = prompt("What is your name?");
+// var nameUpper = name.toUpperCase().slice(0,1);
+// var nameLower = name.toLowerCase().slice(1,name.length);
+// alert("Hello, " + nameUpper + nameLower);
+
+var dogAge = prompt("How old is your dog?");
+var humanAge = ((dogAge - 2) * 4) + 21;
+alert("Your dog is " + humanAge + " years old in human years");
