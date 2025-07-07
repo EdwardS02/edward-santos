9210c72170f7bd483bd30505b1918b95cca84312
EdwardS02 <edwardsantos405.gx@gmail.com>
2025-07-07 00:43:25 -0400

    Life in Weeks practice

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// var user = prompt("Username: ");
+alert// var user = prompt("Username: ");
 // var tweet = prompt("What's on your mind? (max 280 characters)");
 // alert(user + " wrote: " + tweet.slice(0,280) + " (You typed " + tweet.length + " out of 280 characters)");
 
@@ -7,6 +7,17 @@
 // var nameLower = name.toLowerCase().slice(1,name.length);
 // alert("Hello, " + nameUpper + nameLower);
 
-var dogAge = prompt("How old is your dog?");
-var humanAge = ((dogAge - 2) * 4) + 21;
-alert("Your dog is " + humanAge + " years old in human years");
+// var dogAge = prompt("How old is your dog?");
+// var humanAge = ((dogAge - 2) * 4) + 21;
+// alert("Your dog is " + humanAge + " years old in human years");
+window.alert("Let's see how much time you have until you are 90 years old.");
+var age = window.prompt("What's your age?");
+function lifeInWeeks(age) {   
+  var daysLeft = (90 * 365) - (age * 365);
+  var weeksLeft = (90 * 52) - (age * 52);
+  var monthsLeft = (90 * 12) - (age * 12);
+  
+  window.alert("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left.");
+}
+
+lifeInWeeks(age);
