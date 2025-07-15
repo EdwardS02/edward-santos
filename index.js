f3a72229d187e7b9370241cca0ca1246109d19a2
EdwardS02 <edwardsantos405.gx@gmail.com>
2025-07-15 05:39:22 -0400

    bmi calculator and practicing if, else, and return

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert// var user = prompt("Username: ");
+// var user = prompt("Username: ");
 // var tweet = prompt("What's on your mind? (max 280 characters)");
 // alert(user + " wrote: " + tweet.slice(0,280) + " (You typed " + tweet.length + " out of 280 characters)");
 
@@ -11,9 +11,24 @@ alert// var user = prompt("Username: ");
 // var humanAge = ((dogAge - 2) * 4) + 21;
 // alert("Your dog is " + humanAge + " years old in human years");
 
-function lifeInWeeks(age) {   
-  var daysLeft = (90 * 365) - (age * 365);
-  var weeksLeft = (90 * 52) - (age * 52);
-  var monthsLeft = (90 * 12) - (age * 12);
-  console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left.");
-}
+// function lifeInWeeks(age) {   
+//   var daysLeft = (90 * 365) - (age * 365);
+//   var weeksLeft = (90 * 52) - (age * 52);
+//   var monthsLeft = (90 * 12) - (age * 12);
+//   console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left.");
+// }
+
+function bmiCalculator(weight, height) {
+  var bmi = Math.round(weight / (height ** 2));
+  if (bmi < 18.5) {
+    return "Your bmi is " + bmi + " you are underweight.";
+  } else if (bmi < 24.9)  {
+      return "Your bmi is " + bmi + " you are normal weight.";
+  } else if (bmi < 29.9) {
+      return "Your bmi is " + bmi + " you are overweight.";
+  } else if (bmi < 34.9) {
+      return "Your bmi is " + bmi + " you are obese.";
+  } else {
+      return "Your bmi is " + bmi + " you are extremely obese.";
+  }
+ }
