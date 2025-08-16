d4034f971f28900950cd2757d34d49cd9ffeb7c9
EdwardS02 <edwardsantos405.gx@gmail.com>
2025-08-16 18:23:00 -0400

    function practice, leap year challenge

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 // var tweet = prompt("What's on your mind? (max 280 characters)");
 // alert(user + " wrote: " + tweet.slice(0,280) + " (You typed " + tweet.length + " out of 280 characters)");
 
+// --------------------------------------------------------------
+
 // var name = prompt("What is your name?");
 // var nameUpper = name.toUpperCase().slice(0,1);
 // var nameLower = name.toLowerCase().slice(1,name.length);
 // alert("Hello, " + nameUpper + nameLower);
 
+// -_----------------------------------------------------
+
 // var dogAge = prompt("How old is your dog?");
 // var humanAge = ((dogAge - 2) * 4) + 21;
 // alert("Your dog is " + humanAge + " years old in human years");
+
+// -------------------------------------------------------------
 
 // function lifeInWeeks(age) {   
 //   var daysLeft = (90 * 365) - (age * 365);
@@ -17,6 +23,8 @@
 //   var monthsLeft = (90 * 12) - (age * 12);
 //   console.log("You have " + daysLeft + " days, " + weeksLeft + " weeks, and " + monthsLeft + " months left.");
 // }
+
+// -----------------------------------------------------------
 
 // function bmiCalculator(weight, height) {
 //   var bmi = Math.round(weight / (height ** 2));
@@ -33,15 +41,32 @@
 //   }
 //  
 
-var yourName = window.prompt("What is your name?");
-var crushName = window.prompt("What is your crush's name?");
-var randomNumber = Math.random() * 100;
-randomNumber = Math.floor(randomNumber) + 1;
-if (randomNumber > 70) {
-    alert(yourName + " and " + crushName + " have a " + randomNumber + "% probability of falling in love. You are meant to be.")
-} else if (randomNumber < 30) {
-    alert(yourName + " and " + crushName + " have a " + randomNumber + "% probability of falling in love. You are not compatible.")
-} else {
-    alert(yourName + " and " + crushName + " have a " + randomNumber + "% probability of falling in love.");
-};
+// -------------------------------------------------------
+
+// var yourName = window.prompt("What is your name?");
+// var crushName = window.prompt("What is your crush's name?");
+// var randomNumber = Math.random() * 100;
+// randomNumber = Math.floor(randomNumber) + 1;
+// if (randomNumber > 70) {
+//     alert(yourName + " and " + crushName + " have a " + randomNumber + "% probability of falling in love. You are meant to be.")
+// } else if (randomNumber < 30) {
+//     alert(yourName + " and " + crushName + " have a " + randomNumber + "% probability of falling in love. You are not compatible.")
+// } else {
+//     alert(yourName + " and " + crushName + " have a " + randomNumber + "% probability of falling in love.");
+// };
+
+// ----------------------------------------------------------
+
+function isLeap(year) {
+    if (year % 4 != 0 ) {
+        return "Not leap year.";
+    } else if (year % 100 != 0) {
+        return "Leap year.";
+    } else if (year % 400 === 0) {
+        return "Leap year.";
+    } else {
+        return "Not leap year.";
+    }
+}
+
 
